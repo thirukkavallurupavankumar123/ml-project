09782a4362ca0591f8d05c57c3af5c62c6e4ebc9
pavankumar371 <thirukkavallurupavankumar@gmail.com>
2025-08-16 17:26:32 +0530

    modified exception file

--- src/exception.py
+++ src/exception.py
@@ -13,14 +13,11 @@ class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_details(error_message, error_detail)
+        logging.error(self.error_message)
 
     def __str__(self):
         return self.error_message
+        
 
 
-if __name__ == "__main__":
-    try:
-        a = 1 / 0  # this will cause ZeroDivisionError
-    except Exception as e:
-        logging.error("An error occurred: %s", e)
-        raise CustomException(e, sys)
+
